c3c023fd39ab6d78fad5811d1adc3644c4284620
Colby McKim <cm1323@ic.ac.uk>
2024-10-23 21:10:05 +0100

    ..

--- task2/rom.sv
+++ task2/rom.sv
@@ -2,11 +2,11 @@ module rom #(
     parameter   ADDRESS_WIDTH = 8,
                 DATA_WIDTH = 8
 )(
-    input logic     clk,         // input clock
-    input logic[ADDRESS_WIDTH-1:0]     addr1,     // serial data in
-    output logic[DATA_WIDTH-1:0]     dout1,  // serial data out
-    input logic[ADDRESS_WIDTH-1:0]     addr2,     // serial data in
-    output logic[DATA_WIDTH-1:0]     dout2    // serial data out
+    input logic                         clk,         // input clock
+    input logic[ADDRESS_WIDTH-1:0]      addr1,     // serial data in
+    output logic[DATA_WIDTH-1:0]        dout1,  // serial data out
+    input logic[ADDRESS_WIDTH-1:0]      addr2,     // serial data in
+    output logic[DATA_WIDTH-1:0]        dout2    // serial data out
 );
 
     logic [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0];
